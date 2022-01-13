4d4682a48b5e6e7092d539dffb383845f7689895
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2022-01-13 22:54:44 +0300

    feat: add 6kyu kata solution

---
+++ 6kyu/Take a Ten Minute Walk/index.js
@@ -0,0 +1,5 @@
+function isValidWalk(walk) {
+    return walk.length === 10 &&
+        walk.filter(item => item === 'n').length === walk.filter(item => item === 's').length &&
+        walk.filter(item => item === 'w').length === walk.filter(item => item === 'e').length
+}
