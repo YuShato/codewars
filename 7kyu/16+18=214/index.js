1067f8f3ed5ef294f3d6ad383e33fe35e747c287
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2022-01-15 18:06:45 +0300

    feat: add 7kyu  kata  solution

---
+++ 7kyu/16+18=214/index.js
@@ -0,0 +1,19 @@
+function add(num1, num2) {
+    num1 = num1.toString().split("").reverse().join("");
+    num2 = num2.toString().split("").reverse().join("");
+  
+    if (num1.length < num2.length) {
+      [num1, num2] = [num2, num1];
+    }
+  
+    let returnString = "";
+    for (let i = 0; i < num1.length; i++) {
+      let  int1 = parseInt(num1[i]);
+      let int2 = parseInt(num2[i] || 0);
+  
+      returnString = (int1 + int2).toString() + returnString;
+    }
+  
+    return parseInt(returnString);
+  }
+  
---
+++ 7kyu/16+18=214/readme.md
@@ -0,0 +1,7 @@
+In simple terms, our method does not like the principle of carrying over numbers and just writes down every number it calculates :-)
+
+You may assume both integers are positive integers.
+
+Examples
+16+1821426+39515122+811103\large \begin{array}{lll} & 1 & 6 \\ + & 1 & 8 \\ \hline & 2 & 1 4 \\ \end{array} \qquad \large \begin{array}{lll} & 2 & 6 \\ + & 3 & 9 \\ \hline & 5 & 15 \\ \end{array} \qquad \large \begin{array}{lll} & 1 & 2 & 2 \\ + & & 8 & 1 \\ \hline & 1 & 10 & 3 \\ \end{array} 
++
